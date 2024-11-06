c3fc4139faf90d06c47499603f391b3d5585e1aa
donfortune <donfortunet.df@gmail.com>
2024-11-06 11:04:25 +0100

    set up project structure and added templates

---
+++ lms/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def courses(request):
+    return render(request, 'courses.html')
