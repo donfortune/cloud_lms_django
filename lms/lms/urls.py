0e89a53cad97e71344c0f11bf39bad2db16c85e6
Don-Fortune Tangban <74582825+donfortune@users.noreply.github.com>
2025-03-29 21:24:44 +0100

    Update urls.py

--- lms/lms/urls.py
+++ lms/lms/urls.py
@@ -1,7 +1,5 @@
 """
-URL configuration for lms project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
+URL configuration for . For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
